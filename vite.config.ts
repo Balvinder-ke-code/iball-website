1eef3db82d549e8f11c8359cda0b9daba058ae0d
Balvinder Singh <89675452+Balvinder-ke-code@users.noreply.github.com>
2025-08-30 16:48:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "./",
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -35,3 +36,4 @@ export default defineConfig({
     },
   },
 });
+
